11d2ca10cdc6c25d352ea88946273c6a3586b3a2
강보찬 <ebfks0301@gmail.com>
2021-06-05 17:22:32 +0900

    Nest JS 특성 파악

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Param , Redirect, Query} from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
 @Get()
-findAll() : string {
+findAll(@Req() request : Request) : string {
     return 'Cats'
+}
+
+@Get('two')
+CatRet2() : string {
+    console.log('Second Cat' + 1);
+    return 'Cats two'
+}
+
+@Get('docs')
+@Redirect('https://docs.nestjs.com', 302)
+getDocs(@Query('version') version) {
+  if (version && version === '5') {
+    return { url: 'https://docs.nestjs.com/v5/' };
+  }
+}
+
+@Get(':id')
+findOne(@Param() params): string {
+  console.log(params.id);
+  return `This action returns a #${params.id} cat`;
 }
 
 }
